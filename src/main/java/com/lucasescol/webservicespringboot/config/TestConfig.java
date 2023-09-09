aaeb5cd7e53f1567cc07a3d9fd2e18c7f81a5322
Lucas Escolástico <lucasescolastico25@gmail.com>
2023-09-09 17:22:44 -0300

    :sparkles: feat: Created Payment entity and Order-Payment OtO association

--- src/main/java/com/lucasescol/webservicespringboot/config/TestConfig.java
+++ src/main/java/com/lucasescol/webservicespringboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucasescol.webservicespringboot.entities.Category;
 import com.lucasescol.webservicespringboot.entities.Order;
 import com.lucasescol.webservicespringboot.entities.OrderItem;
+import com.lucasescol.webservicespringboot.entities.Payment;
 import com.lucasescol.webservicespringboot.entities.Product;
 import com.lucasescol.webservicespringboot.entities.User;
 import com.lucasescol.webservicespringboot.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
